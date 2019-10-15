d76709ad5e3ed1e5fefa86101fcc0fde5ff08ca1
Krbaio3 <krbaio3@gmail.com>
2019-10-15 21:47:26 +0200

    chore(AXIOS) axios configure

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import Vue from 'vue';
+import VueLogger from 'vuejs-logger';
+import '@/plugins/axios';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import { Http } from '@/namespaces/Http';
+import { options } from './plugins/logger';
+
 
 Vue.config.productionTip = false;
+
+Vue.use(VueLogger, options);
+// tslint:disable-next-line:no-unused-expression
+new Http.ApiJWTService();
+
 
 new Vue({
   router,
