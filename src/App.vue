543bf8a109e0f7dee448771f3fac215412efd20f
Krbaio3 <krbaio3@gmail.com>
2019-10-17 10:17:58 +0200

    feat(Bootstrap-vue) Add dependencies bootstrap-vue

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+   <b-container>
+     <b-nav tabs>
+       <b-nav-item to="/todos">Todos</b-nav-item>
+       <b-nav-item to="/login">Login</b-nav-item>
+       <b-nav-item to="/secret">Secret</b-nav-item>
+     </b-nav>
+     <h1 class="text-center text-muted">Vue Vuex TS</h1>
+     <router-view></router-view>
+   </b-container>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+
 </style>
